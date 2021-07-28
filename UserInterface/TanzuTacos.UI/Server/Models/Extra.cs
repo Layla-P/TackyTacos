16573515dc5d021f4629dec738464e7d4afc636b
layla-p <layla.porter@gmail.com>
2021-07-28 17:06:16 +0100

    good progress made

---
+++ UserInterface/TanzuTacos.UI/Server/Models/Extra.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Threading.Tasks;
+
+namespace TanzuTacos.UI.Server.Models
+{
+
+
+	public class Extra
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public decimal Price { get; set; }
+
+		public string GetFormattedPrice() => Price.ToString("0.00");
+	}
+}
