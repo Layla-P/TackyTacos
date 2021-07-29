6d56a7231944ebd67c92dd0fba6659a81ade759a
Layla-p <layla.porter@gmail.com>
2021-07-29 16:00:19 +0100

    added FoodService API to project

---
+++ Code/Microservices/TanzuTacos.FoodService/Controllers/SpecialsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TanzuTacos.FoodService.Models;
+
+namespace TanzuTacos.FoodService.Controllers
+{
+	[Route("specials")]
+	[ApiController]
+	public class SpecialsController : Controller
+	{
+
+		[HttpGet]
+		public async Task<ActionResult<List<TacoSpecial>>> GetSpecialsAsync()
+		{
+			return await Task.FromResult(GetSpecialsList());
+		}
+
+		private List<TacoSpecial> GetSpecialsList()
+		{
+			return new List<TacoSpecial>
+			{
+				new TacoSpecial
+				{
+					Id = 1,
+					Name ="Fish Tacos",
+					BasePrice = 10M,
+					Description ="Fish on a taco, mate!",
+					ImageUrl ="https://images.theconversation.com/files/299378/original/file-20191030-154707-1n7h8ay.jpg?ixlib=rb-1.1.0&q=30&auto=format&w=600&h=400&fit=crop&dpr=2"
+				},
+				new TacoSpecial
+				{
+					Id = 2,
+					Name ="All the Meat Tacos",
+					BasePrice = 20M,
+					Description ="An Ed pleasing amount of meat on a taco!",
+					ImageUrl ="https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/foodnavigator.com/article/2019/09/06/cutting-meat-consumption-may-cause-serious-harm-academics-warn/10121111-1-eng-GB/Cutting-meat-consumption-may-cause-serious-harm-academics-warn_wrbm_large.jpg"
+				}
+			};
+		}
+	}
+}
---
+++ Code/Microservices/TanzuTacos.FoodService/Models/TacoSpecial.cs
@@ -0,0 +1,17 @@
+﻿namespace TanzuTacos.FoodService.Models
+{
+	public class TacoSpecial
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public decimal BasePrice { get; set; }
+
+		public string Description { get; set; }
+
+		public string ImageUrl { get; set; }
+
+		public string GetFormattedBasePrice() => BasePrice.ToString("0.00");
+	}
+}
